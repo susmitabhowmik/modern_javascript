6a16f5fcdacfca8d29af0af32cabb5bbab1a370e
Susmita Bhowmik <susmita.bhowmik516@gmail.com>
2019-04-01 21:35:18 -0400

    add webpack and tinycolor

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+var moment = require('moment');
+var tinycolor = require('tinycolor2')
+
 console.log("Hello world!")
 
 console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+var color1 = tinycolor("#fff");
+console.log(color1.getBrightness());
